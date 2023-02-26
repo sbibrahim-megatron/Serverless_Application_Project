c4d823f4ac3499c69f41bf8f369e882209b01c47
Ibrahim Sanusi Babansoro <i84202155@china.huawei.com>
2023-02-26 19:45:52 +0100

    Complete project file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = '07ep79xqe4';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-je0q5cuozcvqarmj.us.auth0.com',
+  clientId: 'e48AOVnCsJCi3ctQMxks4OqFgShB0dCE',
   callbackUrl: 'http://localhost:3000/callback'
 };
